418a58f8fbf3109fbeca6161bcd657a41e8bc0e6
BallDonlawat <goodfmlove555@gmail.com>
2019-01-24 19:33:13 +0700

    รวมของบอล

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PaymentComponent } from './payment/payment.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginAddminComponent } from './login-addmin/login-addmin.component';
@@ -7,12 +8,13 @@ import { RegisterComponent } from './register/register.component';
 import { EditContractComponent } from './edit-contract/edit-contract.component';
 import { CustomerComponent } from './customer/customer.component';
 const routes: Routes = [
-  {path:'' , component:LoginAddminComponent},
-  {path:'mainAdmin' , component:MainAdminComponent},
-  {path:'contract' , component:ContractComponent},
-  {path:'editcontract' , component:EditContractComponent},
+  {path: '' , component: LoginAddminComponent},
+  {path: 'mainAdmin' , component: MainAdminComponent},
+  {path: 'contract' , component: ContractComponent},
+  {path: 'editcontract' , component: EditContractComponent},
   {path: 'register', component: RegisterComponent},
-  {path : 'customer', component: CustomerComponent}
+  {path: 'customer', component: CustomerComponent},
+  {path: 'payment' , component: PaymentComponent}
 ];
 
 @NgModule({
